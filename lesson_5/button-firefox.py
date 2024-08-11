8b3a91cc4e2b1099f34e46bf81672464e76aed9a
TanyaAbaldova <168019383+TanyaAbaldova@users.noreply.github.com>
2024-08-11 11:37:45 +0300

    Update button-firefox.py

--- lesson_5/button-firefox.py
+++ lesson_5/button-firefox.py
@@ -4,18 +4,15 @@ from time import sleep
 
 firefox = webdriver.Firefox()
 
-try: 
-    firefox.get("https://the-internet.herokuapp.com/add_remove_elements/")
+ 
+firefox.get("https://the-internet.herokuapp.com/add_remove_elements/")
 
-    for _ in range(5):
+for _ in range(5):
         add_button = firefox.find_element(By.XPATH, '//button[text()="Add Element"]').click()
         
         delete_firefox = firefox.find_elements("xpath",'//button[text()="Delete"]')
+print(f"Фаерфокс {len(delete_firefox)}")
+sleep(5)
 
-    print(f"Фаерфокс {len(delete_firefox)}")
-    sleep(5)
 
-except Exception as ex:
-    print(ex) 
-finally:
-    firefox.quit()
+firefox.quit()
