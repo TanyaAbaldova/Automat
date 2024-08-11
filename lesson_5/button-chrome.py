d221914b534950e72712a4a9f6f89644faca740e
TanyaAbaldova <168019383+TanyaAbaldova@users.noreply.github.com>
2024-08-11 11:37:27 +0300

    Update button-chrome.py

--- lesson_5/button-chrome.py
+++ lesson_5/button-chrome.py
@@ -4,19 +4,16 @@ from time import sleep
 
 chrome = webdriver.Chrome()
 
-try: 
-    chrome.get("https://the-internet.herokuapp.com/add_remove_elements/")
+ 
+chrome.get("https://the-internet.herokuapp.com/add_remove_elements/")
 
-    for _ in range(5):
+for _ in range(5):
         add_button = chrome.find_element(By.XPATH, '//button[text()="Add Element"]').click()
         
     
         delete_chrome = chrome.find_elements(By.XPATH,'//button[text()="Delete"]')
 
-    print(f"Хром {len(delete_chrome)}")
-    sleep(5)
+print(f"Хром {len(delete_chrome)}")
+sleep(5)
 
-except Exception as ex:
-    print(ex) 
-finally:
-    chrome.quit()
+chrome.quit()
